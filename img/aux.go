12f5933c311e9cf41540b30e68abbd0367822888
morpha <v21.leonardo@gmail.com>
2024-01-23 15:38:39 -0300

    trying to implement downscaling on images. No clue yet how to do that.

--- img/aux.go
+++ img/aux.go
@@ -26,7 +26,7 @@ func grayscalePixel(c color.Color) float32 {
 	return ((float32(r) + float32(g) + float32(b)) / 3)
 }
 
-func pixelMatrix(img *image.Image) [][][3]uint8 {
+func imageToMatrix(img *image.Image) [][][3]uint8 {
 	x_max := (*img).Bounds().Max.X
 	y_max := (*img).Bounds().Max.Y
 
@@ -48,7 +48,44 @@ func pixelMatrix(img *image.Image) [][][3]uint8 {
 	return matrix
 }
 
+func matrixToImage(matrix [][][3]uint8) *image.RGBA {
+	img := image.NewRGBA(image.Rect(0, 0, x_max, len(matrix[0])))
+
+	for x := 0; x < img.Bounds().Max.X; x++ {
+		for y := 0; y < img.Bounds().Max.Y; y++ {
+			img.Set(x, y, color.RGBA{matrix[x][y][0], matrix[x][y][1], matrix[x][y][2], 255})
+		}
+	}
+	return img
+}
+
 func shrink(img *image.Image, height int) *image.RGBA {
-	matrix := pixelMatrix(img)
+	matrix := imageToMatrix(img)
+
+	x_max := len(matrix)
+	y_max := len(matrix[0])
+
+	downscalingFactor := height / (*img).Bounds().Max.Y
+
+	width := (*img).Bounds().Max.X * downscalingFactor
+
+	new_matrix := make([][][3]uint8, width)
+	for x := 0; x < width; x++ {
+		new_matrix[x] = make([][3]uint8, height)
+	}
+
+	mappingMatrix := make([][][2]float64, x_max)
+	for x := 0; x < x_max; x++ {
+		mappingMatrix[x] = make([][2]float64, y_max)
+		for y := 0; y < y_max; y++ {
+			mappingMatrix[x][y] = [2]float64{float64(x) * float64(downscalingFactor), float64(y) * float64(downscalingFactor)}
+		}
+	}
+
+	for x := 0; x < x_max; x++ {
+		for y := 0; y < y_max; y++ {
+
+		}
+	}
 
 }
